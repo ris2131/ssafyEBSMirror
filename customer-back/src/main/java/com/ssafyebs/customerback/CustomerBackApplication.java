4fdd670b92bc5f63ad5388e228124aa4eaa56667
DESKTOP-KVCQHCD\multicampus <inseok9876@gmail.com>
2022-10-26 10:52:13 +0900

    first commit

---
+++ customer-back/src/main/java/com/ssafyebs/customerback/CustomerBackApplication.java
@@ -0,0 +1,13 @@
+package com.ssafyebs.customerback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerBackApplication.class, args);
+	}
+
+}
---
+++ customer-back/src/test/java/com/ssafyebs/customerback/CustomerBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafyebs.customerback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
