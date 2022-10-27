39b38502684b941e0c970179d5f27d09a4c2c8f6
이호승 <seong02219@naver.com>
2022-10-27 10:02:25 +0900

    first commit

---
+++ business-be/src/main/java/com/ssafyebs/businessbe/BusinessBeApplication.java
@@ -0,0 +1,13 @@
+package com.ssafyebs.businessbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessBeApplication.class, args);
+	}
+
+}
---
+++ business-be/src/test/java/com/ssafyebs/businessbe/BusinessBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafyebs.businessbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
