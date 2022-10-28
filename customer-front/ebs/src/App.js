0a9770617f717f7bdd316b6dbd661d9527ebd345
ChoiJiSang <cltkddl21@naver.com>
2022-10-28 12:00:50 +0900

    Feat: 네브바 만들기

--- customer-front/ebs/src/App.js
+++ customer-front/ebs/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes,  Navigate } from "react-router-dom";
 import Mainpage from "./pages/mainpage/Mainpage";
 import Ebsnav from "./pages/ebsnav/Ebsnav";
+import Mypage from "./pages/mypage/Mypage";
 
 function App() {
   // const authenticated = useSelector((state) => state.auth.authenticated);
@@ -13,8 +14,8 @@ function App() {
           path="/"
           element={<Ebsnav />} >
           {/* <Routes> */}
-            <Route path="/mainpage" element={<Mainpage />}></Route>
-            
+          <Route path="/mainpage" element={<Mainpage />}></Route>
+          <Route path="/mypage" element={<Mypage />}></Route>  
 
           {/* </Routes> */}
         </Route>
--- customer-front/ebs/src/pages/ebsnav/Ebsnav.js
+++ customer-front/ebs/src/pages/ebsnav/Ebsnav.js
@@ -1,17 +1,20 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Mainpage from '../mainpage/Mainpage';
+import Mypage from '../mypage/Mypage';
 
 const Ebsnav = () => {
   return (
     <>
       <Navbar bg="light" variant="light">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="/mainpage">ebs</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="/mypage">마이페이지</Nav.Link>
+            <Nav.Link href="#features">헤어숍 검색</Nav.Link>
+            <Nav.Link href="#pricing">예약 내역</Nav.Link>
+            <Nav.Link href="#pricing">구독 정보</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
