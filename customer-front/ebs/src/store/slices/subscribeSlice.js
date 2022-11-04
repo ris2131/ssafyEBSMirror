4963d0717a250112a04df1d0cabc989d92b167b9
kofgb <kofgb1234@gmail.com>
2022-11-04 12:21:02 +0900

    Feat : 구독정보 조회 구현중

--- customer-front/ebs/src/store/slices/subscribeSlice.js
+++ customer-front/ebs/src/store/slices/subscribeSlice.js
@@ -1,19 +1,33 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { subscribeApi } from "../../api/subscribeApi"
 
 const initialState = {
     mysubscribe: []
 };
 
+export const getSubscribe = createAsyncThunk(
+    "subscribeSlice/getSubscribe",
+    async(data, { rejectWithValue }) => {
+        try{
+            const res = await subscribeApi.getsubscribeinfo();
+            return res.data;
+        } catch (err) {
+            return rejectWithValue(err.response);
+        }
+    }
+);
+
 const subscribeSlice = createSlice({
     name: "subscribe",
     initialState,
     reducers: {
-        setMysubscribe(state, action) {
+        
+    },
+    extraReducers:{
+        [getSubscribe.fulfilled]:(state, action) =>{
             state.mysubscribe = action.payload
-        },
+        }
     },
 });
-export const {
-    setMysubscribe,
-} = subscribeSlice.actions;
+export const subscribeActions = subscribeSlice.actions;
 export default subscribeSlice.reducer;
