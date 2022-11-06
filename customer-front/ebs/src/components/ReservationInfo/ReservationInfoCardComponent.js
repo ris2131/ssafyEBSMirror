0eb78194760a0156b9430ec2faadeec88af28fd6
kofgb <kofgb1234@naver.com>
2022-11-06 22:51:03 +0900

    Feat : 예약  내역 조회 기능 구현

--- customer-front/ebs/src/components/ReservationInfo/ReservationInfoCardComponent.js
+++ customer-front/ebs/src/components/ReservationInfo/ReservationInfoCardComponent.js
@@ -1,22 +1,24 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
-const ReservationInfocardComponent = () => {
+const ReservationInfocardComponent = (props) => {
   return (
     <Card>
       <Card.Header>
-        <span> 2022.10.22</span>
-        <span>방문 완료</span>
+        <span> 예약 일시 : {props.reservation.reservationDate}</span>
       </Card.Header>
       <Card.Body>
-        <Card.Title>헤어 토모다찌</Card.Title>
+        <Card.Title>헤어샵 이름 : {props.reservation.hairshopName}</Card.Title>
+        <Card.Text>디자이너 이름 : {props.reservation.designerName}</Card.Text>
+        <Card.Text>스타일요청 : {props.reservation.reservationStyle}</Card.Text>
         <Card.Text>
-         수석 디자이너 영우
+          서비스요청 : {props.reservation.reservationService}
         </Card.Text>
+        <Card.Text>기타요청 : {props.reservation.reservationEtc}</Card.Text>
         <Button variant="primary">상세 보기</Button>
       </Card.Body>
     </Card>
   );
-}
+};
 
 export default ReservationInfocardComponent;
--- customer-front/ebs/src/pages/reservation/ReservationInfo.js
+++ customer-front/ebs/src/pages/reservation/ReservationInfo.js
@@ -1,30 +1,42 @@
-import style from './ReservationInfo.module.css';
-import ReservationInfocardComponent from "../../components/ReservationInfo/ReservationInfoCardComponent";   
-import React, { useEffect, useState } from 'react';
+import style from "./ReservationInfo.module.css";
+import ReservationInfocardComponent from "../../components/ReservationInfo/ReservationInfoCardComponent";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getreservations } from '../../store/slices/reservationSlice';
+import { getreservations } from "../../store/slices/reservationSlice";
 
 const ReservationInfo = () => {
-    
-    const myreservation = useSelector((state)=>state.reservation.myreservation)
-    const dispatch = useDispatch();
+  const myreservation = useSelector((state) => state.reservation.myreservation);
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(getreservations())
-        .then((res)=>{console.log(res);});
-    }, []);
+  useEffect(() => {
+    dispatch(getreservations()).then((res) => {
+      console.log(res);
+    });
+  }, []);
 
-    return (
-        <div className={style.Layout}>
-            <div className={style.Title}>
-                <h1>예약 내역</h1>
-            </div>
-                <div className={style.Infocard}>
-                    <ReservationInfocardComponent />
-                </div>
-                <div>{myreservation[0]}</div>
-        </div>
-    );
+  return (
+    <div className={style.Layout}>
+      <div className={style.Title}>
+        <h1>예약 내역</h1>
+      </div>
+      <div className={style.Infocard}>
+        {myreservation.length ? (
+          // <SubscribeInfoComponent subscribe={mysubscribe[0]} />
+          myreservation.map((a, i) => {
+            return (
+              <ReservationInfocardComponent
+                reservation={myreservation[i]}
+                num={i}
+                key={i}
+              />
+            );
+          })
+        ) : (
+          <>예약 정보가 없습니다.</>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default ReservationInfo;
