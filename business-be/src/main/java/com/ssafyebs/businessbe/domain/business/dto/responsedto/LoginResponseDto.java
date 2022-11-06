be4426f49361196a7840e2cce7a467b1fd5ecb26
이호승 <seong02219@naver.com>
2022-11-06 11:21:01 +0900

    Refactor: 로그인 Return값 추가

--- business-be/src/main/java/com/ssafyebs/businessbe/domain/business/dto/responsedto/LoginResponseDto.java
+++ business-be/src/main/java/com/ssafyebs/businessbe/domain/business/dto/responsedto/LoginResponseDto.java
@@ -2,7 +2,9 @@ package com.ssafyebs.businessbe.domain.business.dto.responsedto;
 
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Setter;
 
+@Setter
 public class LoginResponseDto {
     @JsonProperty("hairshop_visible")
     boolean hairShopVisible;
