147a25362364e8374b0249bd7a77a0f5294d8ba0
이호승 <seong02219@naver.com>
2022-11-07 15:51:30 +0900

    Feat: 디자이너 목록 조회 및 추가 페이지 구현

--- business-fe/src/redux/DesignerSlice.js
+++ business-fe/src/redux/DesignerSlice.js
@@ -3,8 +3,20 @@ import { designerApi } from "../shared/designerApi.js";
 // import { imgApi } from "../shared/imgApi";
 
 const initialState = {
-  
+  designers: []
 };
+
+export const getDesigner = createAsyncThunk(
+  "DesignerSlice/designer",
+  async (data, { rejectWithValue }) => {
+    try {
+      const res = await designerApi.getDesigner();
+      return res.data;
+    } catch (err) {
+      return rejectWithValue(err.response);
+    }
+  }
+);
 
 export const addDesigner = createAsyncThunk(
   "DesignerSlice/add-designer",
@@ -31,7 +43,9 @@ const DesignerSlice = createSlice({
     },
   },
   extraReducers: {
-  
+    [getDesigner.fulfilled]: (state, action) => {
+      state.designers = action.payload.data;
+    },
   },
 });
 
