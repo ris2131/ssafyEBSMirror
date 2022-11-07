5794f85ca2c192b29e0db2bf8478e6a68c259f1b
kofgb <kofgb1234@gmail.com>
2022-11-07 12:59:27 +0900

    Feat : 구독상품 리스트 출력 구현중

--- customer-front/ebs/src/components/hairshop/SubscribeComponent.js
+++ customer-front/ebs/src/components/hairshop/SubscribeComponent.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { getitemlist } from "../../store/slices/subscribeSlice";
 
 const SubscribeComponent = () => {
+    const itemlist = useSelector((state) => state.subscribe.itemlist);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getitemlist());
+  }, []);
     return (
         <div>
             구독
