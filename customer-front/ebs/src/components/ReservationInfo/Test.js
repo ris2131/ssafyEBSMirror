7e45249c84fa538868c4e91ba2b2e5148bb3dfbd
ChoiJiSang <cltkddl21@naver.com>
2022-11-09 14:30:30 +0900

    예약 상세 내역 라우터

--- customer-front/ebs/src/components/ReservationInfo/Test.js
+++ customer-front/ebs/src/components/ReservationInfo/Test.js
@@ -4,7 +4,7 @@ import Box from '@mui/material/Box';
 import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-
+import { useNavigate } from "react-router-dom";
 
 
 const Item = styled(Paper)(() => ({
@@ -31,18 +31,39 @@ const theme = createTheme({
 });
 
 export default function Test(props) {
+  console.log(typeof props.reservation.reservationDate)
+  const navigate = useNavigate();
+
+  const data = {
+    businessSeq : props.reservation.businessSeq
+  }
+
+  const move = () => {
+    navigate('/reservation-info-detail', {
+      state: {
+        item: props.reservation
+      }
+    });
+  };
+
+  const moveshop = () => {
+    navigate('/hairshop-info', {state:{...data}});
+  }
+
+  const date1 = new Date(props.reservation.reservationDate);
+  const date2 = new Date();
+  
   return (
             <Box>
                 <Item elevation={24}>
                   
                   <ThemeProvider theme={theme}>
-                    <div>예약 일시 : {props.reservation.reservationDate}</div>
-                    <div>헤어샵 이름 : {props.reservation.hairshopName} </div>
+                    <div> 예약 일시 : {props.reservation.reservationDate}</div>
+                    <div>                              </div>
+                    {date1>date2?<div>방문 예정</div>:<div>방문 완료</div>}
+                    <div onClick={moveshop}>헤어샵 이름 : {props.reservation.hairshopName}</div>
                     <div>디자이너 이름 : {props.reservation.designerName}</div>
-                    <div>스타일요청 : {props.reservation.reservationStyle}</div>
-                    <div>서비스요청 : {props.reservation.reservationService}</div>
-                    <div>기타요청 : {props.reservation.reservationEtc}</div>
-                    <Button variant="contained" color="brown">상세보기</Button>
+                    <Button variant="contained" color="brown" onClick={move}>상세보기</Button>
                   </ThemeProvider>
                 </Item>
             </Box>
