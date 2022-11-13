c04c65211525bbc8f1ffa18479180ced2ed013c9
kofgb <kofgb1234@naver.com>
2022-11-13 17:19:02 +0900

    Feat : 정기결제(단건 결제) 구현

--- customer-front/ebs/src/api/subscribeApi.js
+++ customer-front/ebs/src/api/subscribeApi.js
@@ -3,6 +3,6 @@ import axios from "./index";
 export const subscribeApi = {
     getsubscribeinfo: () => axios.get("/subscribe"),
     getitemlist: (businessSeq) => axios.get(`/subscribe/items/${businessSeq}`),
-    makeSubscribe: (pricingSeq) => axios.post(`/subscribe/${pricingSeq}`),
+    makeSubscribe: (data) => axios.post(`/subscribe`, data),
     getactivesubscribe: () => axios.get("/subscribe/active")
 }
--- customer-front/ebs/src/pages/pay/Approved.js
+++ customer-front/ebs/src/pages/pay/Approved.js
@@ -1,11 +1,39 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation, useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { getitemseq } from "../../store/slices/subscribeSlice";
-import axios from "axios";
+import { Navigate, useLocation, useNavigate } from "react-router-dom";
+import { makeSubscribe } from "../../store/slices/subscribeSlice";
+import { useDispatch } from "react-redux";
 
-var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
-export default function Approved(){
-    console.log(window.location.search);
+
+export default function Approved() {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const params = JSON.parse(window.localStorage.getItem('order'));
+
+    const datas = {
+        cid: params.cid,
+        tid: window.localStorage.getItem('tid'),
+        partner_order_id: params.partner_order_id,
+        partner_user_id: params.partner_user_id,
+        pg_token: window.location.search.split("=")[1]
+    };
+    useEffect(() => {
+        dispatch(makeSubscribe(datas))
+            .then((res) => {
+                if (res.payload.status === "SUCCESS") {
+                    alert("구독 결제에 성공했습니다.");
+                    window.localStorage.removeItem('order');
+                    window.localStorage.removeItem('tid');
+                    navigate('/');
+                } else if (res.payload.status !== "SUCCESS") {
+                    alert("구독에 실패했습니다. 관리자에게 문의바랍니다.")
+                    window.localStorage.removeItem('order');
+                    window.localStorage.removeItem('tid');
+                    navigate('/');
+                }
+            }
+            );
+    }, []);
+
+    
 }
