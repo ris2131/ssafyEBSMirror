7e19bade38cef95a65cd0a5e37f7c6c69c07b1d9
kofgb <kofgb1234@naver.com>
2022-11-16 01:15:53 +0900

    Feat : 각종 구독 관련 페이지 swal 적용

--- customer-front/ebs/src/pages/pay/Canceled.js
+++ customer-front/ebs/src/pages/pay/Canceled.js
@@ -1,11 +1,18 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 export default function Canceled() {
     const navigate = useNavigate();
 
     useEffect(() => {
-        alert("구독 결제를 취소했습니다.");
+        Swal.fire({
+            icon: "info",
+            title: "취소",
+            text: "구독결제를 취소했습니다.\n잠시 후 메인페이지로 이동합니다.",
+            showConfirmButton: false,
+            timer: 1000
+          });
         window.localStorage.removeItem('order');
         window.localStorage.removeItem('tid');
         navigate('/');
--- customer-front/ebs/src/pages/pay/Failed.js
+++ customer-front/ebs/src/pages/pay/Failed.js
@@ -1,11 +1,18 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 export default function Failed() {
     const navigate = useNavigate();
 
     useEffect(() => {
-        alert("구독 결제에 실패했습니다.");
+        Swal.fire({
+            icon: "error",
+            title: "오류",
+            text: "구독 결제에 실패했습니다. 관리자에게 문의해주세요.\n잠시 후 메인페이지로 이동합니다.",
+            showConfirmButton: false,
+            timer: 1000
+        });
         window.localStorage.removeItem('order');
         window.localStorage.removeItem('tid');
         navigate('/');
