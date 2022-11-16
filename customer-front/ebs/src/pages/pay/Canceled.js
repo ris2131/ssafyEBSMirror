50747eca74ed0c3bef994ce4fdf21a47ac10fe68
kofgb <kofgb1234@gmail.com>
2022-11-16 10:02:35 +0900

    Fix : 구독 갱신 취소시 새로고침 되게 수정, 확인창 누르면 바로 이동할 수 있게 수정

--- customer-front/ebs/src/pages/pay/Canceled.js
+++ customer-front/ebs/src/pages/pay/Canceled.js
@@ -10,7 +10,7 @@ export default function Canceled() {
             icon: "info",
             title: "취소",
             text: "구독결제를 취소했습니다.\n잠시 후 메인페이지로 이동합니다.",
-            showConfirmButton: false,
+            showConfirmButton: true,
             timer: 3000
           });
         window.localStorage.removeItem('order');
--- customer-front/ebs/src/pages/pay/Failed.js
+++ customer-front/ebs/src/pages/pay/Failed.js
@@ -10,7 +10,7 @@ export default function Failed() {
             icon: "error",
             title: "오류",
             text: "구독 결제에 실패했습니다. 관리자에게 문의해주세요.\n잠시 후 메인페이지로 이동합니다.",
-            showConfirmButton: false,
+            showConfirmButton: true,
             timer: 3000
         });
         window.localStorage.removeItem('order');
