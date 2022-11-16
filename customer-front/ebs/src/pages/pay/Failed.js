01447173e79c651c706c86f29c35a5924fc35e27
kofgb <kofgb1234@gmail.com>
2022-11-16 09:21:22 +0900

    Fix : swal 알림창 시간 조절

--- customer-front/ebs/src/pages/pay/Failed.js
+++ customer-front/ebs/src/pages/pay/Failed.js
@@ -11,7 +11,7 @@ export default function Failed() {
             title: "오류",
             text: "구독 결제에 실패했습니다. 관리자에게 문의해주세요.\n잠시 후 메인페이지로 이동합니다.",
             showConfirmButton: false,
-            timer: 1000
+            timer: 3000
         });
         window.localStorage.removeItem('order');
         window.localStorage.removeItem('tid');
