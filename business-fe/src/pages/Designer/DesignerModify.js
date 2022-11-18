bbbe0509ec292a31ca18dac75ab9e9ea10969a44
이호승 <seong02219@naver.com>
2022-11-18 16:30:28 +0900

    Style: 디자이너 삭제 버튼 비활성화

--- business-fe/src/pages/Designer/DesignerModify.js
+++ business-fe/src/pages/Designer/DesignerModify.js
@@ -373,12 +373,14 @@ const DesignerModify = () => {
             </TextInputDiv>
           </InputDiv>
           <ConfirmButtonDiv>
-            // <SButton onClick={handleDelete} className={"delete"}>
-            //   디자이너 삭제
-            // </SButton>
+            {/*
+            <SButton onClick={handleDelete} className={"delete"}>
+              디자이너 삭제
+            </SButton>
             <SButton onClick={handleModify} className={"submit"}>
               디자이너 수정
             </SButton>
+            */}
           </ConfirmButtonDiv>
         </DesignerSection>
       </DesignerMain>
