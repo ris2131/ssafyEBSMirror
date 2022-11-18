bd87c5cf9dc05d464af40935adda48817bf937c6
류인석 <inseok9876@gmail.com>
2022-11-18 14:56:06 +0900

    Refactor: 예약에서 캘린더 이전날짜 클릭시 안되게 구현.

--- customer-front/ebs/src/components/hairshop/ReservationComponent.js
+++ customer-front/ebs/src/components/hairshop/ReservationComponent.js
@@ -3,40 +3,55 @@ import Calendar from 'react-calendar';
 import moment from 'moment';
 import './Calendar.css';
 import TimeTableComponent from './TimeTableComponent';
-import { useDispatch, useSelector } from 'react-redux';
-import { businessActions } from '../../store/slices/businessSlice';
+import Swal from 'sweetalert2';
 const ReservationComponent = () => {
 
     const [mode, setMode] = useState(0);
     const [resDate, setDate] = useState("");
 
     const selectDate = (day) => {
-        setDate(moment(day).format("YYYY-MM-DD"))
-        setMode(1)
+        //오늘 날짜
+        //클릭 날짜 console.
+        const clickDay = moment(day).format("YYYY-MM-DD");
+        //오늘 날짜 console. 
+        const today = moment().format("YYYY-MM-DD");
+        //비교
+        const before = moment(clickDay).isBefore(today);
+        
+        //날짜 비교 해서 넘어가기
+        if(before){
+          Swal.fire({
+            'title':'오늘보다 이전날짜에는 예약 불가입니다.',
+            'icon':'error',
+            'iconColor':"#876445",
+            'confirmButtonColor':"#876445",
+            'confirmButtonText': '<div style="font-weight:bold;">OK</div>'
+          });
+        }else{
+          setDate(moment(day).format("YYYY-MM-DD"))
+          setMode(1)
+        }
         
     }
 
 
-    return (
+  return (
+    <div>
+      <h1>예약</h1>
+      {mode === 0 ?(
         <div>
-            <h1>예약</h1>
-            {mode === 0 ?(
-                <div>
-        
-                 <Calendar  onClickDay={selectDate} resDate={resDate} />
-
-                 </div>
-        
-  ) : (mode === 1
-    ? <TimeTableComponent mode={mode} resDate={resDate}/>
-    : (mode === 2)
-    ? <p> 디자이너선택 </p>
-    : <p> ㅍㅍㅍㅍ</p>  )
-  }
-           
-
+          <Calendar onClickDay={selectDate} resDate={resDate} />
         </div>
-    );
+            
+        ) : (mode === 1
+          ? <TimeTableComponent mode={mode} resDate={resDate}/>
+          : (mode === 2)
+          ? <p> 디자이너선택 </p>
+          : <p> 없음</p>  
+        )
+      }
+    </div>
+  );
 };
 
 export default ReservationComponent;
