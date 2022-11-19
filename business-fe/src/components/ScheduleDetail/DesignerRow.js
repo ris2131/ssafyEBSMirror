c8774529eaf8ad01d7171556cd9d67c857886da0
Devillness <78412140+Devillness@users.noreply.github.com>
2022-11-19 20:23:12 +0900

    Style: 예약 상세내용 모달창 스타일 수정

--- business-fe/src/components/ScheduleDetail/DesignerRow.js
+++ business-fe/src/components/ScheduleDetail/DesignerRow.js
@@ -7,6 +7,8 @@ const TimeTableDesignerName = styled.th`
   background-color: antiquewhite;
   padding: 20px 30px;
   color: #A27B5C;
+  font-family: GowunBatang-Regular, sans-serif;
+  font-weight: bold;
 `;
 
 const TimeTableData = styled.td`
@@ -23,7 +25,7 @@ const TimeTableData = styled.td`
 const DesignerRow = (props) => {
 
   const designerSeq = props.designerSeq;
-  const designerName = props.designerName.replace(' ', '_');
+  const designerName = props.designerName;
 
   const time = ["10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30",];
 
